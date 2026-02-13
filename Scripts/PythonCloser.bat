820efde768d81729d50e3992b4d2d0bf6e445ea4
NyveX.gg <holler.soeren3@gmail.com>
2026-02-13 17:03:07 +0100

    Update PythonCloser.bat

--- Scripts/PythonCloser.bat
+++ Scripts/PythonCloser.bat
@@ -1,22 +1,5 @@
 @echo off
-title Kill ALL Python Processes
-echo Killing all Python related processes...
-
-:: Kill by common process names
-taskkill /F /IM python.exe >nul 2>&1
-taskkill /F /IM pythonw.exe >nul 2>&1
-taskkill /F /IM pip.exe >nul 2>&1
-taskkill /F /IM py.exe >nul 2>&1
-
-:: Kill everything that contains "python" in process list
-for /f "skip=1 tokens=2 delims=," %%i in ('wmic process where "CommandLine like '%%python%%'" get ProcessId /format:csv') do (
-    taskkill /F /PID %%i >nul 2>&1
-)
-
-:: Kill virtualenv python too
-for /f "skip=1 tokens=2 delims=," %%i in ('wmic process where "Name like '%%python%%'" get ProcessId /format:csv') do (
-    taskkill /F /PID %%i >nul 2>&1
-)
-
-echo All Python-related processes terminated.
-pause
+taskkill /F /IM python.exe
+taskkill /F /IM pythonw.exe
+for /f "tokens=2" %%i in ('tasklist ^| findstr /I tsunami') do taskkill /F /PID %%i
+exit
