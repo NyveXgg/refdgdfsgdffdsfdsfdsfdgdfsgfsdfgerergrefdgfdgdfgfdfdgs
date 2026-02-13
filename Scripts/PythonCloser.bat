04640055f59fe124785e46934637c963fc2891f2
NyveX.gg <holler.soeren3@gmail.com>
2026-02-13 17:08:09 +0100

    Delete Scripts/PythonCloser.bat

--- Scripts/PythonCloser.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Nuclear Python Killer
-
-for /f "tokens=2 delims=," %%i in ('wmic process get Name^,ProcessId^,CommandLine /format:csv ^| findstr /I python') do (
-    taskkill /F /PID %%i >nul 2>&1
-)
-
-exit
